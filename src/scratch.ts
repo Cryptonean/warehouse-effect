7bde6b3a2bf5db4728032f3559890c0323eeaf19
Cryptonean <faizanvit8@gmail.com>
2025-05-14 20:29:55 +0530

    refactor

--- src/scratch.ts
+++ src/scratch.ts
@@ -1,5 +1,36 @@
+import * as Schema from "@effect/schema/Schema";
 import * as AggreagateRoot from "./AggregateRoot.js";
 
 const ProductAggreagate = AggreagateRoot.AggregateRoot({
   aggregateRootName: "products",
 });
+
+//
+export class ChangeProductNameWithSchema extends Schema.TaggedRequest<ChangeProductNameWithSchema>()(
+  "ChangeProductNameWithSchema",
+  {
+    payload: {
+      ...ProductAggreagate.metadataFields,
+      newName: Schema.NonEmptyString,
+    },
+    success: Schema.Boolean,
+    failure: Schema.Never,
+  }
+) {}
+
+ChangeProductNameWithSchema.make({
+  newName: "New product",
+  _id: "messgae",
+  _aggregateId: "prodcut",
+});
+
+// export class ChangeProductName extends ProductAggreagate.Command<ChangeProductName>()(
+//   {
+//     _tag: "ChangeProductName",
+//     payload: {
+//       newName: Schema.NonEmptyString,
+//     },
+//     success: Schema.Boolean,
+//     failure: Schema.Never,
+//   }
+// ) {}
