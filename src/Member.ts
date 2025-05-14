3b31c8d9e2e708d624b6d653f5becbdaaee53af7
Cryptonean <faizanvit8@gmail.com>
2025-05-14 18:03:31 +0530

    memebr schema

---
+++ src/Member.ts
@@ -0,0 +1,20 @@
+import { Schema } from "effect";
+
+export const MemberIdTypeId = Symbol.for("@@MemberId");
+
+export const MemberId = Schema.UUID.pipe(
+  Schema.annotations({ identifier: "MemberId" })
+);
+
+export const Member = Schema.Struct({
+  memberId: MemberId,
+  name: Schema.NonEmptyString,
+  surname: Schema.NonEmptyString,
+  birthday: Schema.Date,
+}).pipe(
+  Schema.annotations({
+    identifier: "Member",
+  })
+);
+
+export type Member = Schema.Schema.Type<typeof Member>;
